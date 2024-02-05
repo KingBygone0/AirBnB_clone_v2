e5b1a80c41538ade71647a705284d06d4dc367e4
KingBygone0 <KingBygone@icloud.com>
2024-02-05 11:33:10 -0800

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,32 +1,12 @@
 #!/usr/bin/env bash
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+# sets up the web servers for the deployment of web_static
 
-# Create necessary folders if they don't exist
-sudo mkdir -p /data/web_static/{releases/test,shared}
-
-# Create a fake HTML file for testing
-echo "<html>
-	<head>
-	</head>
-	<body>Test Page</body>
-	</html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create or recreate the symbolic link
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership to the ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
-
-
-
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-
-# Restart Nginx
-sudo service nginx restart
-
-# Exit successfully
-exit 0
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
